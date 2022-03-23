efbff168b606bd6a6a035b4f3a2be649c16a29cc
buinm <b.minh96@gmail.com>
2022-03-23 14:40:50 -0700

    Assignment 4 Q1

--- hw4/cs285/agents/mb_agent.py
+++ hw4/cs285/agents/mb_agent.py
@@ -4,6 +4,8 @@ from cs285.policies.MPC_policy import MPCPolicy
 from cs285.infrastructure.replay_buffer import ReplayBuffer
 from cs285.infrastructure.utils import *
 
+import numpy as np
+
 
 class MBAgent(BaseAgent):
     def __init__(self, env, agent_params):
@@ -47,16 +49,15 @@ class MBAgent(BaseAgent):
         num_data_per_ens = int(num_data / self.ensemble_size)
 
         for i in range(self.ensemble_size):
-
             # select which datapoints to use for this model of the ensemble
             # you might find the num_data_per_env variable defined above useful
 
-            observations = # TODO(Q1)
-            actions = # TODO(Q1)
-            next_observations = # TODO(Q1)
+            observations = ob_no[i * num_data_per_ens : (i+1) * num_data_per_ens]
+            actions = ac_na[i * num_data_per_ens : (i+1) * num_data_per_ens]
+            next_observations = next_ob_no[i * num_data_per_ens : (i+1) * num_data_per_ens]
 
             # use datapoints to update one of the dyn_models
-            model =  # TODO(Q1)
+            model =  self.dyn_models[i]
             log = model.update(observations, actions, next_observations,
                                 self.data_statistics)
             loss = log['Training Loss']
